81559cf4c2f3f621dc032374dd2223745272840e
Emily <emjburns@umich.edu>
2016-03-19 15:45:19 -0700

    adding basic angular frontend from https://github.com/airpair/T0021-airpair-angularjs-tutorial

--- run.sh
+++ run.sh
@@ -10,8 +10,12 @@ if [ -z $CATALINA_HOME ]; then
     echo "CATALINA_HOME not set." && exit 1
 fi
 
+# run backend at 8181 (port in $CATALINA_HOME/conf/server.xml)
 catalina stop
 sleep 5
 rm -rf ${CATALINA_HOME}/webapps/mealwheel
 cp ${PRJ}/target/mealwheel.war ${CATALINA_HOME}/webapps
 catalina start
+
+# run and launch front end at 8080
+cd src/main/front-end && http-server -o
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,8 @@
 <html>
 <body>
 <h2>Jersey RESTful Web Application!</h2>
-<p><a href="webresources/myresource">Jersey resource</a>
+<p><a href="api/myresource">Jersey resource</a>
+<p><a href="api/restaurants">restaurants</a>
 <p>Visit the <a href="http://jersey.java.net">Project Jersey website</a>
 for more information on Jersey!
 </body>
